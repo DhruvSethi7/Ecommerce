8f7354f1cfb47e4be4ca14b6b750d1428a0d5bdf
Dhruv <sethi2131@gmail.com>
2023-08-05 23:00:17 +0530

    upto 50

--- ecommerce/flipkart/urls.py
+++ ecommerce/flipkart/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
    path("",views.index,name="FlipkartHome"),
    path("about/",views.about,name="about"),
-path("contact/",views.contact,name="contact"),
+   path("contact/",views.contact,name="contact"),
    path("products/<int:myid>", views.prodView, name="productView"),
+   path("checkout", views.checkout, name="productView"),
 ]
--- ecommerce/flipkart/views.py
+++ ecommerce/flipkart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import product,Contact
+from .models import product,Contact,Orders
 from math import ceil
 def index(request):
         # products=product.objects.all()
@@ -32,6 +32,9 @@ def contact(request):
         desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
+        print('gya ')
+        val=True
+        render(request, 'flipkart/contact.html',{'filled':val})
     return render(request,'flipkart/contact.html')
 
 
@@ -39,3 +42,20 @@ def prodView(request,myid):
     clickedProduct=product.objects.filter(id=myid);
     print(clickedProduct)
     return render(request,'flipkart/prodView.html',{'product':clickedProduct[0]})
+
+def checkout(request):
+    if request.method=="POST":
+        items_json= request.POST.get('itemsJson', '')
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        address=request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city=request.POST.get('city', '')
+        state=request.POST.get('state', '')
+        zip_code=request.POST.get('zip_code', '')
+        phone=request.POST.get('phone', '')
+        order = Orders(items_json= items_json, name=name, email=email, address= address, city=city, state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank=True
+        id=order.order_id
+        return render(request, 'flipkart/checkout.html', {'thank': thank, 'id': id})
+    return render(request, 'flipkart/checkout.html')
